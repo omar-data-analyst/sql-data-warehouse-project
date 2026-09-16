b0d3c779ba070cd962cc12db3cf649feebabc94e
Omar Hussein Analyst‏ <omar.dataanalyst12@gmail.com>
2026-09-16 05:21:42 -1000

    Alter column types in crm_prd_info table
    Changed column types for prd_start_dt and prd_end_dt from DATETIME to DATE in silver.crm_prd_info table.

--- scripts/04_create_silver_tables.sql
+++ scripts/04_create_silver_tables.sql
@@ -50,14 +50,16 @@ IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
 
 CREATE TABLE silver.crm_prd_info (
     prd_id          INT,
+    cat_key			    NVARCHAR(50),
     prd_key         NVARCHAR(50),
     prd_nm          NVARCHAR(100),
     prd_cost        INT,
     prd_line        NVARCHAR(10),
-    prd_start_dt    DATETIME,
-    prd_end_dt      DATETIME,
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
     dwh_create_date    DATETIME2  DEFAULT GETDATE()
 );
+
 
 PRINT 'Created table: silver.crm_prd_info';
 
